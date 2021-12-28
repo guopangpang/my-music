c4b3950d2526593361a11dd8fd86779f80a3825a
guocheng <641688269@qq.com>
2021-12-28 17:10:25 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GlobalStyle } from  './style';
+import { IconStyle } from './assets/iconfont/iconfont';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import routes from './routes/index.js';
+import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
+import { HashRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux'
+import store from './store/index'
+
+
+function App () {
+    return (
+        <Provider store={store}>
+            <HashRouter>
+                <GlobalStyle></GlobalStyle>
+                <IconStyle></IconStyle>
+                { renderRoutes (routes) }
+            </HashRouter>
+        </Provider>
+    )
 }
 
 export default App;
